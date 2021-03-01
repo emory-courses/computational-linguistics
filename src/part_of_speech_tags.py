bcf0e2b0c0e37b281e594fc70df7a4460e05b8bb
Jinho Choi <jinho.choi@emory.edu>
2021-03-01 13:57:48 -0500

    Update part-of-speech tagging

--- src/part_of_speech_tags.py
+++ src/part_of_speech_tags.py
@@ -14,11 +14,14 @@
 # limitations under the License.
 # ========================================================================
 
+import nltk
 import requests
 from pathlib import Path
 from collections import Counter
 from typing import List, Tuple, Dict
 
+
+PREV_DUMMY = '!@#$'
 
 def download_data(path):
     def download(remote_addr: str, local_addr: str):
@@ -76,7 +79,82 @@ def create_uni_pos_dict(data: List[List[Tuple[str, str]]]) -> Dict[str, List[Tup
 
     return model
 
-import nltk
+
+def create_bi_pos_dict(data: List[List[Tuple[str, str]]]) -> Dict[str, List[Tuple[str, float]]]:
+    """
+    :param data: a list of tuple lists where each inner list represents a sentence and every tuple is a (word, pos) pair.
+    :return: a dictionary where the key is the previous POS tag and the value is the list of possible POS tags with probabilities in descending order.
+    """
+    model = dict()
+
+    for sentence in data:
+        for i, (_, curr_pos) in enumerate(sentence):
+            prev_pos = sentence[i-1][1] if i > 0 else PREV_DUMMY
+            model.setdefault(prev_pos, Counter()).update([curr_pos])
+
+    for key, counter in model.items():
+        ts = counter.most_common()
+        total = sum([count for _, count in ts])
+        model[key] = [(pos, count/total) for pos, count in ts]
+
+    return model
+
+
+def predict_uni_pos_dict(uni_pos_dict: Dict[str, List[Tuple[str, float]]], tokens: List[str], pprint=False) -> List[Tuple[str, float]]:
+    def predict(token):
+        t = uni_pos_dict.get(token, None)
+        return t[0] if t else ('XX', 0.0)
+
+    output = [predict(token) for token in tokens]
+    if pprint:
+        for token, t in zip(tokens, output):
+            print('{:<15}{:<8}{:.2f}'.format(token, t[0], t[1]))
+
+    return output
+
+
+def predict_bi_pos_dict(uni_pos_dict: Dict[str, List[Tuple[str, float]]], bi_pos_dict: Dict[str, List[Tuple[str, float]]], tokens: List[str]) -> List[Tuple[str, float]]:
+    output = []
+
+    for i in range(len(tokens)):
+        pos = uni_pos_dict.get(tokens[i], None)
+        if pos is None:
+            pos = bi_pos_dict.get(output[i-1], None)
+        output.append(pos[0] if pos else ('XX', 0.0))
+
+    return output
+
+
+def evaluate_uni_pos(uni_pos_dict: Dict[str, List[Tuple[str, float]]], data: List[List[Tuple[str, str]]]):
+    total, correct = 0, 0
+    for sentence in data:
+        tokens, gold = tuple(zip(*sentence))
+        pred = [t[0] for t in predict_uni_pos_dict(uni_pos_dict, tokens)]
+        total += len(tokens)
+        correct += len([1 for g, p in zip(gold, pred) if g == p])
+    print('{:5.2f}% ({}/{})'.format(100.0 * correct / total, correct, total))
+
+
+def evaluate_bi_pos(uni_pos_dict: Dict[str, List[Tuple[str, float]]], bi_pos_dict: Dict[str, List[Tuple[str, float]]], data: List[List[Tuple[str, str]]]):
+    total, correct = 0, 0
+    for sentence in data:
+        tokens, gold = tuple(zip(*sentence))
+        pred = [t[0] for t in predict_bi_pos_dict(uni_pos_dict, bi_pos_dict, tokens)]
+        total += len(tokens)
+        correct += len([1 for g, p in zip(gold, pred) if g == p])
+    print('{:5.2f}% ({}/{})'.format(100.0 * correct / total, correct, total))
+
+
+def evaluate_nltk(data: List[List[Tuple[str, str]]]):
+    total, correct = 0, 0
+    for sentence in data:
+        tokens, gold = tuple(zip(*sentence))
+        pred = [pos for token, pos in nltk.pos_tag(tokens)]
+        total += len(tokens)
+        correct += len([1 for g, p in zip(gold, pred) if g == p])
+    print('{:5.2f}% ({}/{})'.format(100.0 * correct / total, correct, total))
+
+
 def postag(text):
     tokens = nltk.word_tokenize("And now for something completely different")
     t = nltk.pos_tag(tokens)
@@ -105,8 +183,19 @@ if __name__ == '__main__':
     print(word_count(trn_data))
 
     # unigram model
-    pos_model = create_uni_pos_dict(trn_data)
+    uni_pos_dict = create_uni_pos_dict(trn_data)
 
-    sentence = "I bought a car yesterday that was blue"
-    for token in sentence.split():
-        print(token, pos_model.get(token, None))
+    tokens = "I bought a car yesterday that was blue".split()
+    predict_uni_pos_dict(uni_pos_dict, tokens, True)
+
+    tokens = "Dr. Choi has a good wifi connection from Emory".split()
+    predict_uni_pos_dict(uni_pos_dict, tokens, True)
+
+    dev_data = read_data(path / 'wsj-pos.dev.gold.tsv')
+    evaluate_uni_pos(uni_pos_dict, dev_data)
+
+    # bigram model
+    bi_pos_dict = create_bi_pos_dict(trn_data)
+    evaluate_bi_pos(uni_pos_dict, bi_pos_dict, dev_data)
+
+
