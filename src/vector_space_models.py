4b63837b4c583a0a10036ce586a105e26e79b7e1
Jinho Choi <jinho.choi@emory.edu>
2022-01-25 13:00:17 -0500

    Update VSM

--- src/vector_space_models.py
+++ src/vector_space_models.py
@@ -16,7 +16,13 @@
 
 __author__ = 'Jinho D. Choi'
 
+import json
+from collections import Counter
+from typing import Dict, Tuple
+
+import math
 import requests
+
 
 def download(remote_addr: str, local_addr: str):
     r = requests.get(remote_addr)
@@ -24,8 +30,59 @@ def download(remote_addr: str, local_addr: str):
     fin.write(r.content)
 
 
+def term_frequencies(fables) -> Dict[str, Counter]:
+    def key(t): return t[t.rfind('&') + 1:]
+
+    return {key(fable['source']): Counter(fable['tokens'].split()) for fable in fables}
+
+
+def document_frequencies(fables) -> Dict[str, int]:
+    dfs = Counter()
+    for fable in fables:
+        dfs.update(set(fable['tokens'].split()))
+    return dfs
+
+
+def tf_idfs(fables) -> Dict[str, Dict[str, int]]:
+    tfs = term_frequencies(fables)
+    dfs = document_frequencies(fables)
+    out = dict()
+    D = len(tfs)
+
+    for dkey, term_counts in tfs.items():
+        out[dkey] = {t: tf * math.log(D / dfs[t]) for t, tf in term_counts.items()}
+
+    return out
+
 
 if __name__ == '__main__':
-    aesop_link = 'https://raw.githubusercontent.com/emorynlp/data-collection/master/aesops_fables/dat/aesopfables.json?token=GHSAT0AAAAAABQLGADAWZAYHXOW7LMGBEAIYPZKZSA'
+    # download aesop's fables
+    aesop_link = 'https://raw.githubusercontent.com/emory-courses/computational-linguistics/master/docs/res/aesopfables.json'
     aesop_file = 'res/aesopfables.json'
-    download(aesop_link, aesop_file)
+    # download(aesop_link, aesop_file)
+
+    # read json
+    fables = json.load(open(aesop_file))
+    print(len(fables))
+    for fable in fables[:10]: print(fable['title'])
+
+    # retrieve term frequencies
+    tfs = term_frequencies(fables)
+    print(tfs['Androcles'])
+
+    # retrieve document frequencies
+    dfs = document_frequencies(fables)
+    print(dfs['Lion'], dfs['lion'])
+    for term, count in sorted(dfs.items(), key=lambda x: x[1], reverse=True)[:10]:
+        print(term, count)
+
+    # retrieve TF-IDFs
+    tfidfs = tf_idfs(fables)
+    print(tfidfs['Androcles']['Lion'])
+
+    for t, tf in sorted(tfs['Androcles'].items(), key=lambda x: x[1], reverse=True)[:10]:
+        print(t, tf, math.log(len(tfs) / dfs[t]))
+
+    for t, tfidf in sorted(tfidfs['Androcles'].items(), key=lambda x: x[1], reverse=True)[:10]:
+        print(t, tfidf, dfs[t])
+
